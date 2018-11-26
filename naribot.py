0f176e36dbfad9ab2d0bbe44c583328944bdde10
Narihara <plasmazanberbreaker@gmail.com>
2018-11-26 23:15:42 +0900

    トークン更新デプロイ準備

--- naribot.py
+++ naribot.py
@@ -103,4 +103,4 @@ async def on_message(message):
         send_ms = 'D20ころころ...' + 'あなたのPLお題は' + '[' + odai + ']' + 'に決定しました！'
         await client.send_message(message.channel, send_ms)
 
-client.run("NDc4NTc2NTMyNzA5ODM0NzUz.Dt2LIw._JmgqmNjLXR8XIbX_OoGxWS1XPA")
+client.run("vHunPitOk2WtV-CVlZDkE623uEJx7EwR")
